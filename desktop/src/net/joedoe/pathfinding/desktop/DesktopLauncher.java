2d125fa270c22335578c244a316a28cffde53900
joedoe <d_joe@gmx.net>
2017-11-25 05:46:52 +0100

    window size adjusted

--- desktop/src/net/joedoe/pathfinding/desktop/DesktopLauncher.java
+++ desktop/src/net/joedoe/pathfinding/desktop/DesktopLauncher.java
@@ -2,12 +2,16 @@ package net.joedoe.pathfinding.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+import net.joedoe.GameInfo;
 import net.joedoe.GameMain;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
+	public static void main(String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.title = "Mice in a Maze";
+		config.width = GameInfo.WIDTH;
+		config.height = GameInfo.HEIGHT;
 		new LwjglApplication(new GameMain(), config);
 	}
 }
