7a330299df58355d38d90d004c462e46491ce49e
joedoe <d_joe@gmx.net>
2017-11-25 19:02:18 +0100

    update() adjusted & cleanup

--- core/src/net/joedoe/helpers/Hud.java
+++ core/src/net/joedoe/helpers/Hud.java
@@ -3,10 +3,8 @@ package net.joedoe.helpers;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
@@ -14,34 +12,25 @@ import net.joedoe.GameInfo;
 import net.joedoe.GameMain;
 
 public abstract class Hud {
-    protected Stage stage;
-    protected Viewport viewport;
-    protected Table table;
-    protected Skin skin;
-    protected TextureRegionDrawable panel;
-    protected Label label;
+	protected Stage stage;
+	protected Viewport viewport;
+	protected Table table;
+	protected Skin skin;
 
-    public Hud(GameMain game) {
-        viewport = new FitViewport(GameInfo.WIDTH, GameInfo.HEIGHT, new OrthographicCamera());
-        stage = new Stage(viewport, game.getBatch());
-        Gdx.input.setInputProcessor(stage);
-        skin = new Skin(Gdx.files.internal("fonts/uiskin.json"));
-    }
+	public Hud(GameMain game) {
+		viewport = new FitViewport(GameInfo.WIDTH, GameInfo.HEIGHT, new OrthographicCamera());
+		stage = new Stage(viewport, game.getBatch());
+		Gdx.input.setInputProcessor(stage);
+		skin = new Skin(Gdx.files.internal("fonts/uiskin.json"));
+		skin.getFont("default-font").getData().setScale(1.33f, 1.33f);
+	}
 
-    public Skin getSkin() {
-        return skin;
-    }
+	public Stage getStage() {
+		return this.stage;
+	}
 
-    public Stage getStage() {
-        return this.stage;
-    }
-
-    abstract void createTable();
-
-    abstract void update();
-
-    public void dispose() {
-        skin.dispose();
-        stage.dispose();
-    }
+	public void dispose() {
+		skin.dispose();
+		stage.dispose();
+	}
 }
--- core/src/net/joedoe/helpers/MazeHud.java
+++ core/src/net/joedoe/helpers/MazeHud.java
@@ -3,43 +3,51 @@ package net.joedoe.helpers;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 
+import net.joedoe.GameInfo;
 import net.joedoe.GameMain;
 import net.joedoe.logics.MazeLogic;
 
 public class MazeHud extends Hud {
-	private MazeLogic fight;
-	private Label cheeseXY, mouse1XY, mouse2XY;
+	private MazeLogic mazeLogic;
+	private Label cheese, mouse1, mouse2;
 
-	public MazeHud(GameMain game, MazeLogic fight) {
+	public MazeHud(GameMain game, MazeLogic mazeLogic) {
 		super(game);
-		this.fight = fight;
+		this.mazeLogic = mazeLogic;
 		createTable();
 	}
 
 	void createTable() {
-		cheeseXY = new Label("", skin);
-		mouse1XY = new Label("", skin);
-		mouse2XY = new Label("", skin);
-		String controls = "Place the cheese anywhere in the hallways. Press 'p' for pause/unpause.";
+		cheese = new Label("", skin);
+		mouse1 = new Label("", skin);
+		mouse2 = new Label("", skin);
+		String controls = "Place cheese anywhere in the hallways. Press 'p' to pause/unpause while mice run.";
 		table = new Table(skin);
-		// table.debug();
 		table.bottom().left();
 		table.pad(10);
-		table.setFillParent(true);
-		table.add(controls).top().width(60).left();
-		table.row().padTop(5);
-		table.add("CHEESE:").top().width(60).left();
-		table.add(cheeseXY).padLeft(10).width(100).left();
-		table.add(fight.getMice().get(0).getName().toUpperCase() + ":").padLeft(10).width(70).left();
-		table.add(mouse1XY).padLeft(10).width(100).left();
-		table.add(fight.getMice().get(1).getName().toUpperCase() + ":").padLeft(10).width(70).left();
-		table.add(mouse2XY).padLeft(10).width(100).left();
+		table.add(cheese).padLeft(10).width(200).left();
+		table.add(mouse1).padLeft(10).width(200).left();
+		table.add(mouse2).padLeft(10).width(200).left();
+		table.row().padTop(10);
+		table.add(controls).padLeft(10).width(200).left();
 		stage.addActor(table);
 	}
 
 	public void update() {
-		cheeseXY.setText(fight.getCheeseXY());
-		mouse1XY.setText(fight.getMouse1XY());
-		mouse2XY.setText(fight.getMouse2XY());
+		if (GameInfo.cheeseIsSet) {
+			String cheeseXY = "CHEESE: " + (int) mazeLogic.getCheese()[0] / GameInfo.ONE_TILE + "/"
+					+ (int) mazeLogic.getCheese()[1] / GameInfo.ONE_TILE;
+			cheese.setText(cheeseXY);
+		} else {
+			cheese.setText("CHEESE: ??/??");
+		}
+		String mouse1XY = mazeLogic.getMice().get(0).getName().toUpperCase() + ": "
+				+ (int) mazeLogic.getMice().get(0).getX() / GameInfo.ONE_TILE + "/"
+				+ (int) mazeLogic.getMice().get(0).getY() / GameInfo.ONE_TILE;
+		mouse1.setText(mouse1XY);
+		String mouse2XY = mazeLogic.getMice().get(1).getName().toUpperCase() + ": "
+				+ (int) mazeLogic.getMice().get(1).getX() / GameInfo.ONE_TILE + "/"
+				+ (int) mazeLogic.getMice().get(1).getY() / GameInfo.ONE_TILE;
+		mouse2.setText(mouse2XY);
 	}
 }
