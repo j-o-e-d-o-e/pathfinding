4d88ce30d2794d3c65fe663adc203afc2426bccb
joedoe <d_joe@gmx.net>
2017-11-25 05:48:02 +0100

    Animation for Mouse added

--- core/src/net/joedoe/entities/Actor.java
+++ core/src/net/joedoe/entities/Actor.java
@@ -1,45 +1,57 @@
 package net.joedoe.entities;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 import net.joedoe.GameInfo;
 
 public abstract class Actor {
-	protected Texture texture;
+	protected Animation<TextureRegion>[] texture;
 	protected float x, y;
-	protected int direction;
+	protected int direction = 1;
 	protected String name;
 	public boolean hasMoved;
 
-	public Actor(Texture texture, String name, float x, float y, int strength) {
-		this.texture = texture;
+	public Actor(String path, String name, float x, float y, int strength) {
+		this.texture = initializeTextureRegion(path);
 		this.name = name;
 		this.x = x;
 		this.y = y;
 	}
 
+	Animation<TextureRegion>[] initializeTextureRegion(String path) {
+		TextureRegion[][] textureRegions = TextureRegion.split(new Texture(path), 38, 26);
+		@SuppressWarnings("unchecked")
+		Animation<TextureRegion>[] animation = new Animation[textureRegions.length];
+		for (int i = 0; i < textureRegions.length; i++) {
+			animation[i] = new Animation<TextureRegion>(10f / 30f, textureRegions[i]);
+		}
+		return animation;
+	}
+
 	public void move() {
 		switch (direction) {
-		case 1: // N
+		case 0: // N
 			y += GameInfo.ONE_TILE;
 			if (y > GameInfo.HEIGHT - GameInfo.ONE_TILE) {
 				y = GameInfo.HEIGHT - GameInfo.ONE_TILE;
 			}
 			break;
-		case 2: // W
+		case 1: // W
 			x -= GameInfo.ONE_TILE;
 			if (x < 0) {
 				x = 0;
 			}
 			break;
-		case 3: // S
+		case 2: // S
 			y -= GameInfo.ONE_TILE;
 			if (y < 0) {
 				y = 0;
 			}
 			break;
-		case 4: // E
+		case 3: // E
 			x += GameInfo.ONE_TILE;
 			if (x > GameInfo.WIDTH - GameInfo.ONE_TILE) {
 				x = GameInfo.WIDTH - GameInfo.ONE_TILE;
@@ -48,8 +60,8 @@ public abstract class Actor {
 		}
 	}
 
-	public void render(SpriteBatch batch) {
-		batch.draw(texture, x, y, GameInfo.ONE_TILE, GameInfo.ONE_TILE);
+	public void render(SpriteBatch batch, float elapsedTime) {
+		batch.draw(texture[direction].getKeyFrame(elapsedTime, true), x, y, GameInfo.ONE_TILE, GameInfo.ONE_TILE);
 	}
 
 	public String getName() {
@@ -60,12 +72,8 @@ public abstract class Actor {
 		this.name = name;
 	}
 
-	public Texture getTexture() {
+	public Animation<TextureRegion>[] getTexture() {
 		return texture;
-	}
-
-	public void setTexture(String path) {
-		this.texture = new Texture(path);
 	}
 
 	public float getX() {
--- core/src/net/joedoe/entities/Mouse.java
+++ core/src/net/joedoe/entities/Mouse.java
@@ -2,20 +2,20 @@ package net.joedoe.entities;
 
 import com.badlogic.gdx.ai.pfa.DefaultGraphPath;
 import com.badlogic.gdx.ai.pfa.indexed.IndexedAStarPathFinder;
-import com.badlogic.gdx.graphics.Texture;
+
 import net.joedoe.logics.pathfinding.Graph;
 import net.joedoe.logics.pathfinding.ManhattanHeuristic;
 import net.joedoe.logics.pathfinding.Node;
 
 public class Mouse extends Actor {
-    public Graph graph;
+	public Graph graph;
     public DefaultGraphPath<Node> path;
     public IndexedAStarPathFinder<Node> pathfinder;
     public ManhattanHeuristic heuristic;
     public int pathIndex = 1;
 
     public Mouse(String name, float x, float y, int strength) {
-        super(new Texture("entities/enemy.png"), name, x, y, strength);
+        super("entities/mouse.png", name, x, y, strength);
         path = new DefaultGraphPath<Node>();
         heuristic = new ManhattanHeuristic();
     }
