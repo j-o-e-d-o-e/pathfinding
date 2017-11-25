c95ccf8d55c5d1db3627435055c2e5daf43401f1
joedoe <d_joe@gmx.net>
2017-11-25 05:49:50 +0100

    pause/unpause, custom cursor et al. added

--- core/src/net/joedoe/screens/Maze.java
+++ core/src/net/joedoe/screens/Maze.java
@@ -4,11 +4,10 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Cursor;
+import com.badlogic.gdx.graphics.Cursor.SystemCursor;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Animation;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.Vector3;
@@ -24,9 +23,10 @@ public class Maze extends MyScreen {
 	private OrthogonalTiledMapRenderer renderer;
 	private MazeLogic maze;
 	private MazeHud hud;
-	private float mouseTimer;
-	private Animation<TextureRegion>[] mouse;
+	private Cursor cursor;
+	private float mouseTimer, elapsedTime;
 	private Texture cheese;
+	private float cheeseX, cheeseY;
 
 	public Maze(GameMain game) {
 		super(game);
@@ -34,64 +34,69 @@ public class Maze extends MyScreen {
 		hud = new MazeHud(this.game, maze);
 		renderer = new OrthogonalTiledMapRenderer(maze.getMap());
 		cheese = new Texture("entities/cheese.png");
-		TextureRegion[][] mouseTextures = TextureRegion.split(new Texture("entities/mouse.png"), 38, 26);
-		mouse = new Animation[mouseTextures.length];
-		for (int i = 0; i < mouseTextures.length; i++) {
-			mouse[i] = new Animation<TextureRegion>(10f / 30f, mouseTextures[i]);
-		}
-		Cursor cursor = Gdx.graphics.newCursor(new Pixmap(Gdx.files.internal("entities/cheese.png")), 0, 31);
-		Gdx.graphics.setCursor(cursor);
+		cursor = Gdx.graphics.newCursor(new Pixmap(Gdx.files.internal("entities/cheese.png")), 0, 31);
+
 	}
 
 	void handleInput() {
-		if (!GameInfo.isPaused) {
+		if (!GameInfo.cheeseIsSet) {
 			if (Gdx.input.justTouched()) {
 				Vector3 vector = new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0);
 				camera.unproject(vector);
-				String cheeseXY = (int) (vector.x / GameInfo.ONE_TILE) + "/" + (int) (vector.y / GameInfo.ONE_TILE);
-				maze.setCheeseXY(cheeseXY);
-				GameInfo.isSet = true;
-			} else if (Gdx.input.isKeyJustPressed(Input.Keys.P)) {
+				int tileX = (int) vector.x / GameInfo.ONE_TILE;
+				int tileY = (int) vector.y / GameInfo.ONE_TILE;
+				float cheeseX = (int) (vector.x / GameInfo.ONE_TILE) * GameInfo.ONE_TILE;
+				float cheeseY = (int) (vector.y / GameInfo.ONE_TILE) * GameInfo.ONE_TILE;
+				if (maze.tileIsAccessible(tileX, tileY) && !maze.collidesWithActor(cheeseX, cheeseY)) {
+					this.cheeseX = cheeseX;
+					this.cheeseY = cheeseY;
+					maze.setCheeseX(this.cheeseX);
+					maze.setCheeseY(this.cheeseY);
+					maze.setCheeseXY(tileX + "/" + tileY);
+					GameInfo.cheeseIsSet = true;
+				}
+				for (Mouse mouse : maze.getMice()) {
+					mouse.hasMoved = false;
+				}
+			}
+		} else if (Gdx.input.isKeyJustPressed(Input.Keys.P)) {
+			if (GameInfo.isPaused) {
+				GameInfo.isPaused = false;
+			} else if (!GameInfo.isPaused) {
 				GameInfo.isPaused = true;
 			}
-		} else if (GameInfo.isPaused) {
-			if (Gdx.input.isKeyJustPressed(Input.Keys.P)) {
-				GameInfo.isPaused = false;
-			}
-		}
-	}
-
-	void update() {
-		if (!GameInfo.isSet) {
-			handleInput();
-			for (Mouse mouse : maze.getMice()) {
-				mouse.hasMoved = false;
-			}
-		} else if (mouseTimer >= 0.3) {
-			maze.updateMice();
-			mouseTimer = 0;
 		}
 	}
 
 	@Override
 	public void render(float delta) {
 		mouseTimer += delta;
+		elapsedTime += delta;
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		if (!GameInfo.isPaused) {
-			update();
+		handleInput();
+		if (!GameInfo.isPaused && mouseTimer >= 0.2) {
+			maze.updateMice();
+			mouseTimer = 0;
 		}
 		renderer.render();
 		renderer.setView(camera);
 		game.getBatch().begin();
-		// maze.getPlayer().render(game.getBatch());
+		if (GameInfo.cheeseIsSet) {
+			Gdx.graphics.setSystemCursor(SystemCursor.Arrow);
+			game.getBatch().draw(cheese, cheeseX, cheeseY);
+		} else if (!GameInfo.cheeseIsSet) {
+			Gdx.graphics.setCursor(cursor);
+		}
 		for (Mouse enemy : maze.getMice()) {
-			enemy.render(game.getBatch());
+			enemy.render(game.getBatch(), elapsedTime);
 		}
 		game.getBatch().end();
 		game.getBatch().setProjectionMatrix(hud.getStage().getCamera().combined);
-		debug();
+		if (GameInfo.cheeseIsSet) {
+			debug();
+		}
 		hud.update();
 		hud.getStage().draw();
 		hud.getStage().act();
@@ -101,11 +106,11 @@ public class Maze extends MyScreen {
 		ShapeRenderer shapeRenderer = new ShapeRenderer();
 		shapeRenderer.setProjectionMatrix(game.getBatch().getProjectionMatrix());
 		shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
-		// PATH OF ENEMY_1 IN RED
+		// PATH OF MOUSE_1 IN RED
 		for (Node node : maze.getMice().get(0).path) {
 			node.render(shapeRenderer, Color.RED);
 		}
-		// PATH OF ENEMY_2 IN YELLOW
+		// PATH OF MOUSE_2 IN YELLOW
 		for (Node node : maze.getMice().get(1).path) {
 			node.render(shapeRenderer, Color.YELLOW);
 		}
